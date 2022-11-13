42ee8cfc0a1a4609a6e6adfb55d6f6f58dc4286a
DmytroDehtiarov <dmytro.dehtiarov02@gmail.com>
2022-11-13 00:02:41 +0000

    added site files

---
+++ design/scripts/index.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+	autoScrolling: true,
+	navigation: true,
+	anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
+	navigationTooltips: ['Home', 'About', 'Services', 'Gallery', 'Contact'],
+	scrollingSpeed: 1200,
+	
+})
+
